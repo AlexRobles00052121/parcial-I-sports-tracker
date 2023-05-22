ac138e512290665ee93cb2e263335cafc0cf26a8
AlexRobles00052121 <00052121@uca.edu.sv>
2023-05-22 15:04:48 -0600

    :memo: add: view model and connect te app with te repository

--- sports-tracker/app/src/main/java/com/example/sports_tracker/SportApp.kt
+++ sports-tracker/app/src/main/java/com/example/sports_tracker/SportApp.kt
@@ -1,7 +1,12 @@
 package com.example.sports_tracker
 
 import android.app.Application
+import com.example.sports_tracker.data.sports
+import com.example.sports_tracker.repositories.SportRepository
 
 class SportApp: Application() {
 
+    val sportRepository: SportRepository by lazy {
+        SportRepository(sports)
+    }
 }
